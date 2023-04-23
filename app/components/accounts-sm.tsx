f23c066d077ff57a0f942d112d368c4716f4c578
AnanthuTD <ananthutd2021@gmail.com>
2023-04-23 15:03:19 +0530

    added for suggestion list

---
+++ app/components/accounts-sm.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import SwitchButton from "./brightBlueButton";
+
+function AccountSM() {
+    return (
+        <>
+            <div className="flex my-3 cursor-pointer">
+                <Image
+                    src="/images/pro-pic.jpg"
+                    width={40}
+                    height={40}
+                    alt=""
+                    className="rounded-full"
+                />
+                <p className="flex items-center mx-4 text-sm">username</p>
+                <SwitchButton text="Follow" />
+            </div>
+        </>
+    );
+}
+
+export default AccountSM;
