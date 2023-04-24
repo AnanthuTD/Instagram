257fb774edf5fcd19e89bdd4a923fbab3147e850
AnanthuTD <ananthutd2021@gmail.com>
2023-04-24 20:13:34 +0530

    reformated

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
+// Import necessary components and libraries
 "use client";
 import SideBar from "./side_bar";
 import Stories from "./components/stories/stories";
 import Login from "./login";
 import Signup from "./signup";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import Account_suggestion from "./components/right_pannel/accountSuggestion";
 import Profile from "./menu/profile";
+import Create from "./menu/create";
+import Messages from "./menu/messages";
+import Notifications from "./menu/notifications";
+import Search from "./menu/search";
+import Explore from "./menu/explore";
+import Reels from "./menu/reels";
 
+// Define a type for the menu state
 interface MenuState {
-    home: boolean;
+	home: boolean;
 	profile: boolean;
 	create: boolean;
 	search: boolean;
@@ -17,30 +25,35 @@ interface MenuState {
 	messages: boolean;
 	notifications: boolean;
 	reels: boolean;
-  }
+}
 
-export default function Home(userData: object) {
+// Define the Home component
+export default function Home() {
+	// Define state variables
 	const [user, setuser] = useState<object | undefined>(undefined);
 	const [loading, setLoading] = useState(true);
 	const [signup, setSignup] = useState(false);
 	const [menu, setMenu] = useState<MenuState>({
-        home: true,
-        profile: false,
-        create: false,
-        search: false,
-        explore: false,
-        messages: false,
-        notifications: false,
-        reels: false,
-      });
+		home: true,
+		profile: false,
+		create: false,
+		search: false,
+		explore: false,
+		messages: false,
+		notifications: false,
+		reels: false,
+	});
 
+	// Define an effect to handle user authentication
 	useEffect(() => {
+		// If a user object is available, set the user cookie and stop loading
 		if (user) {
 			console.log("user true");
 			Cookies.set("user", JSON.stringify(user));
 			setLoading(false);
 			setSignup(false);
 		} else {
+			// If no user object is available, try to get the user cookie
 			let cookieuser: object | undefined = undefined;
 			const userCookie = Cookies.get("user");
 
@@ -52,11 +65,14 @@ export default function Home(userData: object) {
 				}
 			}
 
+			// Set the user state variable to the cookie user object
 			setuser(cookieuser);
 		}
 
+		// Stop loading
 		setLoading(false);
 
+		// Define a function to fetch the user from the backend API
 		const fetchUser = async () => {
 			const response = await fetch("api/accounts/login/");
 			const data = await response.json();
@@ -66,11 +82,13 @@ export default function Home(userData: object) {
 			}
 		};
 
+		// If no user object is available, fetch the user from the API
 		if (!user) {
 			fetchUser();
 		}
 	}, [user]);
 
+	// Define the Home component's content
 	let homeComponent = (
 		<>
 			<div className="w-3/6 p-5 flex justify-end">
@@ -82,12 +100,12 @@ export default function Home(userData: object) {
 		</>
 	);
 
-    useEffect(() => {
-        console.log(menu);
-        
-    }, [menu])
-    
+	// Debug the menu state
+	useEffect(() => {
+		console.log(menu);
+	}, [menu]);
 
+	// Render different content depending on the authentication and menu state
 	if (!user && loading) {
 		return <div>Loding</div>;
 	} else if (user) {
@@ -98,7 +116,12 @@ export default function Home(userData: object) {
 				</div>
 				{menu.home ? homeComponent : null}
 				{menu.profile ? <Profile /> : null}
-                
+				{menu.create ? <Create /> : null}
+				{menu.explore ? <Explore /> : null}
+				{menu.messages ? <Messages /> : null}
+				{menu.notifications ? <Notifications /> : null}
+				{menu.reels ? <Reels /> : null}
+				{menu.search ? <Search /> : null}
 			</main>
 		);
 	} else if (signup) {
