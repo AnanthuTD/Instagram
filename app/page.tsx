a1eda53ae07ecaf596a821d911103ca3fa07b7a6
AnanthuTD <ananthutd2021@gmail.com>
2023-04-24 19:40:47 +0530

    added menu state

--- app/page.tsx
+++ app/page.tsx
@@ -2,80 +2,116 @@
 import SideBar from "./side_bar";
 import Stories from "./components/stories/stories";
 import Login from "./login";
-import Signup from "./signup/page";
-import { useState, useEffect } from "react";
+import Signup from "./signup";
+import { useState, useEffect, useCallback } from "react";
 import Cookies from "js-cookie";
 import Account_suggestion from "./components/right_pannel/accountSuggestion";
+import Profile from "./menu/profile";
+
+interface MenuState {
+    home: boolean;
+	profile: boolean;
+	create: boolean;
+	search: boolean;
+	explore: boolean;
+	messages: boolean;
+	notifications: boolean;
+	reels: boolean;
+  }
 
 export default function Home(userData: object) {
-    const [user, setuser] = useState<object | undefined>(undefined);
-    const [loading, setLoading] = useState(true);
-    const [signup, setSignup] = useState(false);
+	const [user, setuser] = useState<object | undefined>(undefined);
+	const [loading, setLoading] = useState(true);
+	const [signup, setSignup] = useState(false);
+	const [menu, setMenu] = useState<MenuState>({
+        home: true,
+        profile: false,
+        create: false,
+        search: false,
+        explore: false,
+        messages: false,
+        notifications: false,
+        reels: false,
+      });
+
+	useEffect(() => {
+		if (user) {
+			console.log("user true");
+			Cookies.set("user", JSON.stringify(user));
+			setLoading(false);
+			setSignup(false);
+		} else {
+			let cookieuser: object | undefined = undefined;
+			const userCookie = Cookies.get("user");
+
+			if (userCookie !== undefined) {
+				try {
+					cookieuser = JSON.parse(userCookie);
+				} catch (error) {
+					cookieuser = undefined;
+				}
+			}
+
+			setuser(cookieuser);
+		}
+
+		setLoading(false);
+
+		const fetchUser = async () => {
+			const response = await fetch("api/accounts/login/");
+			const data = await response.json();
+			if (data.status) {
+				setuser(data.user);
+				Cookies.set("user", JSON.stringify(data.user));
+			}
+		};
+
+		if (!user) {
+			fetchUser();
+		}
+	}, [user]);
+
+	let homeComponent = (
+		<>
+			<div className="w-3/6 p-5 flex justify-end">
+				<Stories />
+			</div>
+			<div className="w-2/6 p-5">
+				<Account_suggestion />
+			</div>
+		</>
+	);
 
     useEffect(() => {
-        if (user) {
-            console.log("user true");
-            Cookies.set("user", JSON.stringify(user));
-            setLoading(false);
-            setSignup(false);
-        } else {
-            let cookieuser: object | undefined = undefined;
-            const userCookie = Cookies.get("user");
+        console.log(menu);
+        
+    }, [menu])
+    
 
-            if (userCookie !== undefined) {
-                try {
-                    cookieuser = JSON.parse(userCookie);
-                } catch (error) {
-                    cookieuser = undefined;
-                }
-            }
-
-            setuser(cookieuser);
-        }
-
-        setLoading(false);
-
-        const fetchUser = async () => {
-            const response = await fetch("api/accounts/login/");
-            const data = await response.json();
-            if (data.status) {
-                setuser(data.user);
-                Cookies.set("user", JSON.stringify(data.user));
-            }
-        };
-
-        if (!user) {
-            fetchUser();
-        }
-    }, [user]);
-
-    if (!user && loading) {
-        return <div>Loding</div>;
-    } else if (user) {
-        return (
-            <main className="flex min-h-screen flex-row bg-black">
-                <div className="w-1/6 p-5 border-r border-side_bar_border">
-                    <SideBar />
-                </div>
-                <div className="w-3/6 p-5 flex justify-end">
-                    <Stories />
-                </div>
-                <div className="w-2/6 p-5">
-                    <Account_suggestion />
-                </div>
-            </main>
-        );
-    } else if (signup) {
-        return (
-            <main className="bg-white flex min-h-screen flex-row justify-center">
-                <Signup setUser={setuser} />
-            </main>
-        );
-    } else {
-        return (
-            <main className="bg-white flex min-h-screen flex-row justify-center">
-                <Login setUser={setuser} setSignup={setSignup} />
-            </main>
-        );
-    }
+	if (!user && loading) {
+		return <div>Loding</div>;
+	} else if (user) {
+		return (
+			<main className="flex min-h-screen flex-row bg-black">
+				<div className="w-1/6 p-5 border-r border-side_bar_border">
+					<SideBar menu={menu} setMenu={setMenu} />
+				</div>
+				{menu.home ? homeComponent : null}
+				{menu.profile ? <Profile /> : null}
+                
+			</main>
+		);
+	} else if (signup) {
+		return (
+			<main className="bg-white flex min-h-screen flex-row justify-center">
+				<Signup setUser={setuser} />
+			</main>
+		);
+	} else {
+		return (
+			<main className="bg-white flex min-h-screen flex-row justify-center">
+				<Login setUser={setuser} setSignup={setSignup} />
+			</main>
+		);
+	}
 }
