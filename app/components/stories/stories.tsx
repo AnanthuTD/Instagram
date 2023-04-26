f795828f3734c17869dddc149f6d59a6d9b12f00
AnanthuTD <ananthutd2021@gmail.com>
2023-04-26 18:08:47 +0530

    added scrolling

--- app/components/stories/stories.tsx
+++ app/components/stories/stories.tsx
@@ -1,60 +1,107 @@
+import ArrowLeft from "./arrowLeft";
 import ArrowRight from "./arrowRight";
 import Rings from "./rings";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Stories() {
-  let data = Array(9).fill(null);
-  const stories = data.map((_, index) => (
-    <>
-      <Rings avatar="/images/default_profile.png" />
-      <div className="px-2"></div>
-    </>
-  ));
+	const [scrollLeft, setscrollLeft] = useState(false);
+	const [scrollRight, setscrollRight] = useState(false);
 
-  function handleClick() {
-    const container = document.getElementById("scroll-container");
-    const containerWidth = container?.getBoundingClientRect().width || 0;
-    const contentWidth = container?.scrollWidth || 0;
-    const scrollAmount = containerWidth / 2;
+	let data = Array(9).fill(null);
+	const stories = data.map((_, index) => (
+		<>
+			<Rings avatar="/images/default_profile.png" />
+			<div className="px-2"></div>
+		</>
+	));
 
-    if (container && contentWidth - container.scrollLeft > containerWidth) {
-      container.scrollBy({ left: scrollAmount, behavior: "smooth" });
-    }
-  }
+	useEffect(() => {
+		if (data.length > 8) {
+			setscrollRight(true);
+		}
+	}, []);
 
-  return (
-    <>
-      <div
-        className="flex relative"
-        style={{ height: "fit-content", maxWidth: "685px" }}
-      >
-        {/* scrollable div */}
-        <div
-          className="overflow-x-scroll overflow-y-hidden no-scrollbar pointer-events-auto"
-          style={{
-            height: "fit-content",
-            maxWidth: "685px",
-           
-          }}
-          id="scroll-container"
-        >
-          <div className="flex" style={{ height: "fit-content" }}>
-            {stories}
-            <Rings />
-          </div>
-        </div>
-        {/* more stories... */}
-        <div
-          className="flex items-center absolute top-0 bottom-0 right-10 z-10 cursor-pointer"
-          onClick={() => {
-            handleClick();
-          }}
-        >
-          {data.length > 8 ? <ArrowRight /> : null}
-        </div>
-      </div>
-    </>
-  );
+	function handleClickRight() {
+		const container = document.getElementById("scroll-container");
+		const containerWidth = container?.getBoundingClientRect().width || 0;
+		const contentWidth = container?.scrollWidth || 0;
+		const scrollAmount = containerWidth / 2;
+
+		if (container && contentWidth - container.scrollLeft > containerWidth) {
+			container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+			if (scrollLeft !== true) {
+				setscrollLeft(true);
+			}
+			if (contentWidth - container.scrollLeft === contentWidth) {
+				setscrollRight(false);
+			}
+		}
+	}
+
+	function handleClickLeft() {
+		const container = document.getElementById("scroll-container");
+		const containerWidth = container?.getBoundingClientRect().width || 0;
+		const contentWidth = container?.scrollWidth || 0;
+		const scrollAmount = containerWidth / 2;
+
+		if (container && container.scrollLeft !== 0) {
+
+			container.scrollBy({ left: -scrollAmount, behavior: "smooth" })
+			if (scrollRight !== true) {
+				setscrollRight(true);
+			}
+
+			if (container.scrollLeft + containerWidth >= contentWidth) {
+				setscrollLeft(false);
+			}
+		}
+	}
+
+	return (
+		<>
+			<div
+				className="flex relative"
+				style={{ height: "fit-content", maxWidth: "685px" }}
+			>
+				{/* scroll left */}
+				{scrollLeft ? (
+					<div
+						className="flex items-center absolute top-0 bottom-0 left-10 z-10 cursor-pointer"
+						onClick={() => {
+							handleClickLeft();
+						}}
+					>
+						{data.length > 8 ? <ArrowLeft /> : null}
+					</div>
+				) : null}
+				{/* stories */}
+				<div
+					className="overflow-x-scroll overflow-y-hidden no-scrollbar pointer-events-auto"
+					style={{
+						height: "fit-content",
+						maxWidth: "685px",
+					}}
+					id="scroll-container"
+				>
+					<div className="flex" style={{ height: "fit-content" }}>
+						{stories}
+						<Rings />
+					</div>
+				</div>
+				{/* more stories... */}
+				{scrollRight ? (
+					<div
+						className="flex items-center absolute top-0 bottom-0 right-10 z-10 cursor-pointer"
+						onClick={() => {
+							handleClickRight();
+						}}
+					>
+						{data.length > 8 ? <ArrowRight /> : null}
+					</div>
+				) : null}
+			</div>
+		</>
+	);
 }
 
 export default Stories;
