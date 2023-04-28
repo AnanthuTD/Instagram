b4e45c6579eaae496de5fea4b95c550602155071
AnanthuTD <ananthutd2021@gmail.com>
2023-04-29 01:00:19 +0530

    added handle [Click,FileInputChange,ClickOutside,Submit

--- app/menu/create.tsx
+++ app/menu/create.tsx
@@ -1,29 +1,78 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState, useRef, useEffect } from "react";
 import { MenuState } from "../Interfaces";
 import PictureAndVideo from "../components/create/pictureAndVideo";
+import { NullLiteral } from "typescript";
 interface CreateProps {
-	setMenu: Dispatch<
-		SetStateAction<MenuState>
-	>
-	menu: MenuState
+	setMenu: Dispatch<SetStateAction<MenuState>>;
+	menu: MenuState;
 }
 
-function create({setMenu, menu}:CreateProps) {
+function create({ setMenu, menu }: CreateProps) {
+	const [selectedFile, setSelectedFile] = useState<File | null>(null);
+	const fileInputRef = useRef<HTMLInputElement | null>(null);
+	const formSubmitRef = useRef<HTMLFormElement | null>(null);
+	const elevatedDiv = useRef<HTMLDivElement | null>(null);
+
+	const handleClick = () => {
+		if (fileInputRef.current) fileInputRef.current.click();
+	};
+
+	const handleFileInputChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		if (event.target.files && event.target.files.length > 0) {
+			setSelectedFile(event.target.files[0]);
+		}
+	};
+
+	useEffect(() => {
+		function handleClickOutside(this: Document, ev: MouseEvent) {
+			if (
+				elevatedDiv &&
+				!elevatedDiv.current?.contains(ev.target as Node)
+			)
+				setMenu({ ...menu, create: false });
+		}
+
+		document.addEventListener("click", handleClickOutside);
+
+		return () => {
+			document.removeEventListener("click", handleClickOutside);
+		};
+	}, []);
+
+	const handleSubmit = () => {
+		if (formSubmitRef.current) {
+			let file = new FormData(formSubmitRef.current)
+		}
+	};
+
 	return (
 		<>
 			<div
 				className="absolute inset-0 flex justify-center items-center"
 				style={{ minHeight: "380px", minWidth: "380px" }}
-				onClick={() =>
-					setMenu({...menu, 'create': false} )
-				}
 			>
-				<div className="aspect-square h-5/6 bg-elevated rounded-xl shadow-md transform transition-all duration-500">
+				<div
+					className="aspect-square h-5/6 bg-elevated rounded-xl shadow-md transform transition-all duration-500"
+					ref={elevatedDiv}
+				>
 					<div
 						className="flex justify-center text-white font-bold p-3 border-b w-full"
 						style={{ borderColor: "#3d3d3d" }}
 					>
-						Create new post
+						<div className="w-1/4"></div>
+						<div className="w-2/4 justify-center flex">
+							Create new post
+						</div>
+						<div className="w-1/4 flex justify-end">
+							<button
+								className="rounded text-blue-500 text-sm p-1"
+								onClick={handleSubmit}
+							>
+								Next
+							</button>
+						</div>
 					</div>
 					<div
 						className="flex w-full justify-center"
@@ -32,9 +81,26 @@ function create({setMenu, menu}:CreateProps) {
 						<div className="space-y-3 justify-center flex flex-col items-center">
 							<PictureAndVideo fill="white" />
 							<p>Drag photos and videos here</p>
-							<button className="rounded-lg bg-blue-500 text-white p-2 font-bold text-sm">
+							<button
+								className="rounded-lg bg-blue-500 text-white p-2 font-bold text-sm"
+								onClick={handleClick}
+							>
 								Select from computer
 							</button>
+							<form
+								action=""
+								method="post"
+								ref={formSubmitRef}
+								encType="multipart/form-data"
+							>
+								<input
+									ref={fileInputRef}
+									type="file"
+									className="hidden"
+									name="Select from computer"
+									onChange={handleFileInputChange}
+								/>
+							</form>
 						</div>
 					</div>
 				</div>
