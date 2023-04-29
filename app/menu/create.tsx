af26d334adea4c25eef16ece219a592098be98a2
AnanthuTD <ananthutd2021@gmail.com>
2023-04-29 12:37:37 +0530

    added preview of the selected image

--- app/menu/create.tsx
+++ app/menu/create.tsx
@@ -1,7 +1,7 @@
 import { Dispatch, SetStateAction, useState, useRef, useEffect } from "react";
 import { MenuState } from "../Interfaces";
 import PictureAndVideo from "../components/create/pictureAndVideo";
-import { NullLiteral } from "typescript";
+import Image from "next/image";
 interface CreateProps {
 	setMenu: Dispatch<SetStateAction<MenuState>>;
 	menu: MenuState;
@@ -12,6 +12,7 @@ function create({ setMenu, menu }: CreateProps) {
 	const fileInputRef = useRef<HTMLInputElement | null>(null);
 	const formSubmitRef = useRef<HTMLFormElement | null>(null);
 	const elevatedDiv = useRef<HTMLDivElement | null>(null);
+	const [preview, setPreview] = useState<string | ArrayBuffer | null>(null);
 
 	const handleClick = () => {
 		if (fileInputRef.current) fileInputRef.current.click();
@@ -43,26 +44,44 @@ function create({ setMenu, menu }: CreateProps) {
 
 	const handleSubmit = () => {
 		if (formSubmitRef.current) {
-			let file = new FormData(formSubmitRef.current)
+			let file = new FormData(formSubmitRef.current);
 		}
 	};
+
+	useEffect(() => {
+		if (selectedFile) {
+			console.log(selectedFile);
+
+			let reader = new FileReader();
+			reader.onload = () => {
+				setPreview(reader.result);
+			};
+			reader.readAsDataURL(selectedFile);
+		}
+	}, [selectedFile]);
 
 	return (
 		<>
 			<div
 				className="absolute inset-0 flex justify-center items-center"
-				style={{ minHeight: "380px", minWidth: "380px" }}
+				style={{
+					minHeight: "380px",
+					minWidth: "380px",
+					height: "100%",
+					zIndex: 15,
+				}}
 			>
 				<div
-					className="aspect-square h-5/6 bg-elevated rounded-xl shadow-md transform transition-all duration-500"
+					className="aspect-square h-5/6 bg-elevated rounded-xl shadow-md transform transition-all duration-500 p-0"
 					ref={elevatedDiv}
 				>
+					{/* header */}
 					<div
 						className="flex justify-center text-white font-bold p-3 border-b w-full"
-						style={{ borderColor: "#3d3d3d" }}
+						style={{ borderColor: "#3d3d3d", height: "10%" }}
 					>
 						<div className="w-1/4"></div>
-						<div className="w-2/4 justify-center flex">
+						<div className="w-2/4 justify-center flex items-center">
 							Create new post
 						</div>
 						<div className="w-1/4 flex justify-end">
@@ -74,34 +93,48 @@ function create({ setMenu, menu }: CreateProps) {
 							</button>
 						</div>
 					</div>
+					{/* content */}
 					<div
-						className="flex w-full justify-center"
-						style={{ height: "inherit" }}
+						className="flex w-full justify-center overflow-hidden rounded-b-xl"
+						style={{ height: "90%" }}
 					>
-						<div className="space-y-3 justify-center flex flex-col items-center">
-							<PictureAndVideo fill="white" />
-							<p>Drag photos and videos here</p>
-							<button
-								className="rounded-lg bg-blue-500 text-white p-2 font-bold text-sm"
-								onClick={handleClick}
-							>
-								Select from computer
-							</button>
-							<form
-								action=""
-								method="post"
-								ref={formSubmitRef}
-								encType="multipart/form-data"
-							>
-								<input
-									ref={fileInputRef}
-									type="file"
-									className="hidden"
-									name="Select from computer"
-									onChange={handleFileInputChange}
+						{!selectedFile ? (
+							<div className="space-y-3 justify-center flex flex-col items-center">
+								<PictureAndVideo fill="white" />
+								<p>Drag photos and videos here</p>
+								<button
+									className="rounded-lg bg-blue-500 text-white p-2 font-bold text-sm"
+									onClick={handleClick}
+								>
+									Select from computer
+								</button>
+								<form
+									action=""
+									method="post"
+									ref={formSubmitRef}
+									encType="multipart/form-data"
+								>
+									<input
+										ref={fileInputRef}
+										type="file"
+										className="hidden"
+										name="Select from computer"
+										onChange={handleFileInputChange}
+									/>
+								</form>
+							</div>
+						) : (
+							preview && (
+								<img
+									src={preview as string}
+									alt={`Preview of ${selectedFile?.name}`}
+									style={{
+										maxWidth: "max-content",
+										maxHeight: "max-content",
+									}}
 								/>
-							</form>
-						</div>
+							)
+						)}
 					</div>
 				</div>
 			</div>
