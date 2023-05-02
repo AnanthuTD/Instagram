3bec125c3010cc658c02d6c2926acdb103574507
AnanthuTD <ananthutd2021@gmail.com>
2023-05-03 00:58:50 +0530

    added timeDifference function

--- app/components/home/Stories&Posts.tsx
+++ app/components/home/Stories&Posts.tsx
@@ -45,6 +45,33 @@ function StoriesPosts() {
 		}
 	}, [comment]);
 
+	function timeDifference(time_stamp: Date): string {
+		const dateObj = new Date(time_stamp);
+		const currentDate = new Date();
+
+		const differenceYears =
+			currentDate.getFullYear() - dateObj.getFullYear();
+		if (differenceYears) return differenceYears + "y";
+
+		const differenceMonths = currentDate.getMonth() - dateObj.getMonth();
+		if (differenceMonths) return differenceMonths + "m";
+
+		const differenceDays = currentDate.getDate() - dateObj.getDate();
+		if (differenceDays) return differenceDays + "d";
+		
+
+		const differenceHours = currentDate.getHours() - dateObj.getHours();
+		if (differenceHours) return differenceHours + "h";
+
+		const differenceMins = currentDate.getMinutes() - dateObj.getMinutes();
+		if (differenceMins) return differenceMins + "m";
+
+		const differenceSecs = currentDate.getSeconds() - dateObj.getSeconds();
+		if (differenceSecs) return differenceSecs + "s";
+
+		return "";
+	}
+
 	return (
 		<>
 			<div className="" style={{ maxWidth: "685px" }}>
@@ -76,8 +103,11 @@ function StoriesPosts() {
 												backgroundColor: "gray",
 											}}
 										></span>
-										<span style={{ color: "gray" }}>
-											{post.time_stamp}
+										<span
+											className="text-xs"
+											style={{ color: "gray" }}
+										>
+											{timeDifference(post.time_stamp)}
 										</span>
 									</div>
 									<div className="">
