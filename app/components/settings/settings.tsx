c2b40f16083f75ceb2cd8fc5fe73e8df732e7490
AnanthuTD <ananthutd2021@gmail.com>
2023-05-04 15:28:16 +0530

    settings page

---
+++ app/components/settings/settings.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function settings() {
+  return (
+    <>
+        <div className="flex h-full w-full">
+            <span className='text-white font-bold'>Settings</span>
+        </div>
+    </>
+  )
+}
+
+export default settings
