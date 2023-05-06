1109b106d60d26f3e1627961c6899f37112e1964
AnanthuTD <ananthutd2021@gmail.com>
2023-05-06 20:07:57 +0530

    settings page

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -1,9 +1,53 @@
-import Accounts from "@/app/components/right_pannel/accounts";
-import AccountSM from "./accounts-sm";
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { UserState} from "../../../utils/Interfaces"
+import { fetchCSRF} from "../../../utils/fetch_csrf"
+import {useUserContext}  from "../../components/context/userContext"
+
 
 function settings() {
+	// useState
+	const [bio, setBio] = useState("");
+	const [website, setWebsite] = useState("");
+	const [gender, setGender] = useState("");
+
+	// context
+	const { user, setUser } = useUserContext();
+
+	async function handleSubmit(): Promise<void> {
+		const csrfToken = await fetchCSRF();
+		const Response = await fetch("api/accounts/profile/", {
+			headers: {
+				"content-type": "application/json",
+				"X-CSRFToken": csrfToken as string,
+			},
+			method: "post",
+			body: JSON.stringify({
+				website: website,
+				bio: bio,
+				gender: gender,
+			}),
+		});
+		let data = await Response.json();
+		if (data.status && user) {
+			let newUser: UserState = {
+				...user,
+				bio: bio,
+				gender: gender,
+				website: website,
+			};
+			setUser(newUser);
+		}
+	}
+
+	useEffect(() => {
+		if (!user) return;
+		setBio(user.bio);
+		setWebsite(user.website);
+		setGender(user.gender);
+	}, []);
+
 	return (
 		<>
 			<main className="h-full w-full">
@@ -44,77 +88,88 @@ function settings() {
 										</button>
 									</div>
 								</div>
-								<div className="mt-5">
-									<form className="space-y-3">
-										<div className="flex space-x-10">
-											<aside className="w-1/5 flex justify-end">
-												<label
-													htmlFor="website"
-													className="font-extrabold"
-												>
-													Website
-												</label>
-											</aside>
-											<div className="w-3/5">
-												<input
-													className="bg-transparent border rounded w-full p-1"
-													type="url"
-													name="website"
-													id=""
-													placeholder="website"
-												/>
-											</div>
+								<div className="mt-5 space-y-3">
+									<div className="flex space-x-10">
+										<aside className="w-1/5 flex justify-end">
+											<label
+												htmlFor="website"
+												className="font-extrabold"
+											>
+												Website
+											</label>
+										</aside>
+										<div className="w-3/5">
+											<input
+												className="bg-transparent border rounded w-full p-1"
+												type="url"
+												name="website"
+												id=""
+												placeholder="website"
+												onChange={(e) =>
+													setWebsite(e.target.value)
+												}
+												value={website}
+											/>
 										</div>
-										<div className="flex space-x-10">
-											<aside className="w-1/5 flex justify-end">
-												<label
-													htmlFor="bio"
-													className="font-extrabold"
-												>
-													Bio
-												</label>
-											</aside>
-											<div className="w-3/5">
-												<textarea
-													className="bg-transparent border rounded w-full p-1"
-													name="bio"
-													id=""
-													placeholder="Bio"
-												/>
-											</div>
+									</div>
+									<div className="flex space-x-10">
+										<aside className="w-1/5 flex justify-end">
+											<label
+												htmlFor="bio"
+												className="font-extrabold"
+											>
+												Bio
+											</label>
+										</aside>
+										<div className="w-3/5">
+											<textarea
+												className="bg-transparent border rounded w-full p-1"
+												name="bio"
+												id=""
+												placeholder="Bio"
+												onChange={(e) =>
+													setBio(e.target.value)
+												}
+												value={bio}
+											/>
 										</div>
-										<div className="flex space-x-10">
-											<aside className="w-1/5 flex justify-end">
-												<label
-													htmlFor="website"
-													className="font-extrabold"
-												>
-													Gender
-												</label>
-											</aside>
-											<div className="w-3/5">
-												<input
-													className="bg-transparent border rounded w-full p-1"
-													type="text"
-													name="gender"
-													id=""
-													placeholder="Gender"
-												/>
-											</div>
+									</div>
+									<div className="flex space-x-10">
+										<aside className="w-1/5 flex justify-end">
+											<label
+												htmlFor="website"
+												className="font-extrabold"
+											>
+												Gender
+											</label>
+										</aside>
+										<div className="w-3/5">
+											<input
+												className="bg-transparent border rounded w-full p-1"
+												type="text"
+												name="gender"
+												id=""
+												placeholder="Gender"
+												onChange={(e) =>
+													setGender(e.target.value)
+												}
+												value={gender}
+											/>
 										</div>
-										<div className="flex space-x-10">
-											<aside className="w-1/5 flex justify-end"></aside>
-											<div className="w-3/5">
-												<button
-													type="submit"
-													name="profile"
-													className="bg-brightBlue text-white rounded-lg px-3"
-												>
-													submit
-												</button>
-											</div>
+									</div>
+									<div className="flex space-x-10">
+										<aside className="w-1/5 flex justify-end"></aside>
+										<div className="w-3/5">
+											<button
+												type="submit"
+												name="profile"
+												className="bg-brightBlue text-white rounded-lg px-3"
+												onClick={() => handleSubmit()}
+											>
+												submit
+											</button>
 										</div>
-									</form>
+									</div>
 								</div>
 							</div>
 						</div>
