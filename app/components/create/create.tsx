dd5bd958d55d009b9954a2f1b7bb4ab9f8ea41dc
AnanthuTD <ananthutd2021@gmail.com>
2023-05-24 21:03:31 +0530

    added menu to select upload post and stories

--- app/components/create/create.tsx
+++ app/components/create/create.tsx
@@ -1,49 +1,25 @@
-import { useState, useRef, useEffect } from "react";
-import SmileIcon from "../posts/smileIcon";
-import PictureAndVideo from "./pictureAndVideo";
-import AvatarUsername from "../avatar_username";
-import LocationIcon from "./locationIcon";
-import Preview from "./preview";
-import React from "react";
-import { useMenuContext } from "../context/menuContext";
-import { fetchCSRF } from "../../../utils/fetch_csrf";
+import React, { useState, useRef, useEffect } from "react";
+
+import Post from "./post";
+import Stories from "./stories";
 
 function create({
 	setCreate,
 }: {
 	setCreate: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-	const [selectedFile, setSelectedFile] = useState<File | null>(null);
-	const [preview, setPreview] = useState<string | null>(null);
-	const [formData, setFormData] = useState<FormData>(new FormData());
-	const [location, setLocation] = useState("");
-	const [caption, setCaption] = useState("");
-	const [submit, setSubmit] = useState(true);
-	const [fileAdded, setFileAdded] = useState(false);
-	const [video, setVideo] = useState("");
-
 	// ref
-	const fileInputRef = useRef<HTMLInputElement | null>(null);
-	const formSubmitRef = useRef<HTMLFormElement | null>(null);
 	const elevatedDiv = useRef<HTMLDivElement | null>(null);
-
-	const handleClick = () => {
-		if (fileInputRef.current) fileInputRef.current.click();
-	};
-
-	const handleFileInputChange = (
-		event: React.ChangeEvent<HTMLInputElement>
-	) => {
-		if (event.target.files && event.target.files.length > 0) {
-			setSelectedFile(event.target.files[0]);
-		}
-	};
+	const [post, setPost] = useState(false);
+	const [stories, setStories] = useState(false);
+	const mainDivRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
 		function handleClickOutside(this: Document, ev: MouseEvent) {
 			if (
-				elevatedDiv &&
-				!elevatedDiv.current?.contains(ev.target as Node)
+				!elevatedDiv.current?.contains(ev.target as Node) &&
+				mainDivRef.current &&
+				mainDivRef.current.contains(ev.target as Node)
 			) {
 				setCreate(false);
 			}
@@ -56,158 +32,52 @@ function create({
 		};
 	}, []);
 
-	async function postData() {
-		const csrfToken = await fetchCSRF();
-		const Response = await fetch("/api/post", {
-			method: "POST",
-			body: formData,
-			headers: {
-				"X-CSRFToken": csrfToken,
-			},
-		});
-		const data = await Response.json();
-		if (data.status === true) setCreate(false);
-		else alert("posting failed");
-	}
+	useEffect(() => {
+		if (post) setStories(false);
+	}, [post]);
 
 	useEffect(() => {
-		if (selectedFile && !formData.has("file")) {
-			setFileAdded(true);
-			formData.append("file", selectedFile);
-			return;
-		}
-		if (formData.has("file")) {
-			formData.append("caption", caption);
-			formData.append("location", location);
-			// posting data to server
-			postData();
-		}
-	}, [submit, formData]);
-
-	useEffect(() => {
-		if (selectedFile) {
-			let reader = new FileReader();
-			reader.onload = () => {
-				setPreview(reader.result as string);
-			};
-			reader.readAsDataURL(selectedFile);
-
-			const url = URL.createObjectURL(selectedFile);
-			setVideo(url);
-		}
-	}, [selectedFile]);
+		if (stories) setPost(false);
+	}, [stories]);
 
 	return (
 		<>
 			<div
-				className="absolute inset-0 flex justify-center items-center bg-blackBlur"
+				className="absolute inset-0 flex items-center justify-center bg-blackBlur"
 				style={{
 					minHeight: "380px",
 					minWidth: "380px",
 					height: "100%",
 					zIndex: 15,
 				}}
+				ref={mainDivRef}
 			>
-				<div
-					className=" h-5/6 bg-elevated rounded-xl overflow-hidden shadow-md transform transition-all duration-500 p-0"
-					ref={elevatedDiv}
-				>
-					{/* header */}
-					<div
-						className="flex justify-center text-white font-bold p-3 border-b w-full"
-						style={{ borderColor: "#3d3d3d", height: "10%" }}
-					>
-						<div className="w-1/4"></div>
-						<div className="w-2/4 justify-center flex items-center">
-							Create new post
-						</div>
-						<div className="w-1/4 flex justify-end">
-							{selectedFile ? (
-								<button
-									className="rounded text-blue-500 text-sm p-1"
-									onClick={() => setSubmit(!submit)}
-								>
-									Next
-								</button>
-							) : null}
-						</div>
-					</div>
-					{/* content */}
-					<div className="flex" style={{ height: "90%" }}>
-						<div className="aspect-square h-full flex justify-center overflow-hidden">
-							{!selectedFile ? (
-								<div className="space-y-3 justify-center flex flex-col items-center">
-									<PictureAndVideo fill="white" />
-									<p>Drag photos and videos here</p>
-									<button
-										className="rounded-lg bg-blue-500 text-white p-2 font-bold text-sm"
-										onClick={handleClick}
-									>
-										Select from computer
-									</button>
-									<form
-										ref={formSubmitRef}
-										className=""
-										style={{ visibility: "hidden" }}
-										encType="multipart/form-data"
-									>
-										<input
-											ref={fileInputRef}
-											type="file"
-											name="Select from computer"
-											onChange={handleFileInputChange}
-										/>
-									</form>
-								</div>
-							) : // preview
-							selectedFile && preview ? (
-								<Preview
-									preview={preview}
-									name={selectedFile.name}
-									type={selectedFile.type}
-								/>
-							) : null}
-						</div>
-						{fileAdded ? (
+				{!(stories || post) ? (
+					<>
+						<div className="flex flex-col items-center space-y-3 rounded-lg bg-elevated p-5">
 							<div
-								className="p-3 space-y-3 border-l"
-								style={{
-									borderColor: "#3d3d3d",
-								}}
+								className="cursor-pointer rounded px-4 py-2 font-bold text-primaryText outline outline-1 outline-border_grey hover:outline-white"
+								onClick={() => setPost(true)}
 							>
-								<AvatarUsername
-									height={30}
-									width={30}
-									className="text-sm font-bold text-white"
-								/>
-								<textarea
-									name="caption"
-									id="caption"
-									cols={30}
-									rows={10}
-									placeholder="Write a caption..."
-									className="bg-transparent resize-none outline-none"
-									required={false}
-									onChange={(e) => setCaption(e.target.value)}
-								></textarea>
-								<SmileIcon width={20} height={20} />
-								<div className="flex justify-between">
-									<input
-										type="text"
-										placeholder="Add location"
-										className="border-none bg-transparent outline-none"
-										onChange={(e) =>
-											setLocation(e.target.value)
-										}
-									/>
-									<span>
-										<LocationIcon />
-									</span>
-								</div>
+								POST
 							</div>
-						) : null}
+							<div
+								className="cursor-pointer rounded px-4 py-2 font-bold text-primaryText outline outline-1 outline-border_grey hover:outline-white"
+								onClick={() => setStories(true)}
+							>
+								STORIES
+							</div>
+						</div>
+					</>
+				) : (
+					<div
+						className=" h-5/6 transform overflow-hidden rounded-xl bg-elevated p-0 shadow-md transition-all duration-500"
+						ref={elevatedDiv}
+					>
+						{post || stories? <Post setCreate={setCreate} post={post} stories={stories}/> : null}
+						
 					</div>
-				</div>
+				)}
 			</div>
 		</>
 	);
---
+++ app/components/create/post.tsx
@@ -0,0 +1,183 @@
+import { useState, useRef, useEffect } from "react";
+import SmileIcon from "../posts/smileIcon";
+import PictureAndVideo from "./pictureAndVideo";
+import AvatarUsername from "../avatar_username";
+import LocationIcon from "./locationIcon";
+import Preview from "./preview";
+import React from "react";
+import { fetchCSRF } from "../../../utils/fetch_csrf";
+
+function post({
+	setCreate,post,stories
+}: {
+	setCreate: React.Dispatch<React.SetStateAction<boolean>>;
+	post: boolean;
+	stories: boolean;
+}) {
+	const [selectedFile, setSelectedFile] = useState<File | null>(null);
+	const [preview, setPreview] = useState<string | null>(null);
+	const [formData, setFormData] = useState<FormData>(new FormData());
+	const [location, setLocation] = useState("");
+	const [caption, setCaption] = useState("");
+	const [submit, setSubmit] = useState(true);
+	const [fileAdded, setFileAdded] = useState(false);
+	const [video, setVideo] = useState("");
+
+	// ref
+	const fileInputRef = useRef<HTMLInputElement | null>(null);
+	const formSubmitRef = useRef<HTMLFormElement | null>(null);
+
+	const url = post?"/api/post":"/api/post/stories/";
+
+	const handleClick = () => {
+		if (fileInputRef.current) fileInputRef.current.click();
+	};
+
+	const handleFileInputChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		if (event.target.files && event.target.files.length > 0) {
+			setSelectedFile(event.target.files[0]);
+		}
+	};
+
+	async function postData() {
+		const csrfToken = await fetchCSRF();
+		const Response = await fetch(url, {
+			method: "POST",
+			body: formData,
+			headers: {
+				"X-CSRFToken": csrfToken,
+			},
+		});
+		const data = await Response.json();
+		if (data.status === true) setCreate(false);
+		else alert("posting failed");
+	}
+
+	useEffect(() => {
+		if (selectedFile && !formData.has("file")) {
+			setFileAdded(true);
+			formData.append("file", selectedFile);
+			return;
+		}
+		if (formData.has("file")) {
+			formData.append("caption", caption);
+			formData.append("location", location);
+			// posting data to server
+			postData();
+		}
+	}, [submit, formData]);
+
+	useEffect(() => {
+		if (selectedFile) {
+			let reader = new FileReader();
+			reader.onload = () => {
+				setPreview(reader.result as string);
+			};
+			reader.readAsDataURL(selectedFile);
+
+			const url = URL.createObjectURL(selectedFile);
+			setVideo(url);
+		}
+	}, [selectedFile]);
+	return (
+		<>
+			{/* header */}
+			<div
+				className="flex w-full justify-center border-b p-3 font-bold text-white"
+				style={{ borderColor: "#3d3d3d", height: "10%" }}
+			>
+				<div className="w-1/4"></div>
+				<div className="flex w-2/4 items-center justify-center">
+					Create new post
+				</div>
+				<div className="flex w-1/4 justify-end">
+					{selectedFile ? (
+						<button
+							className="rounded p-1 text-sm text-blue-500"
+							onClick={() => setSubmit(!submit)}
+						>
+							Next
+						</button>
+					) : null}
+				</div>
+			</div>
+			{/* content */}
+			<div className="flex" style={{ height: "90%" }}>
+				<div className="flex aspect-square h-full justify-center overflow-hidden">
+					{!selectedFile ? (
+						<div className="flex flex-col items-center justify-center space-y-3">
+							<PictureAndVideo fill="white" />
+							<p>Drag photos and videos here</p>
+							<button
+								className="rounded-lg bg-blue-500 p-2 text-sm font-bold text-white"
+								onClick={handleClick}
+							>
+								Select from computer
+							</button>
+							<form
+								ref={formSubmitRef}
+								className=""
+								style={{ visibility: "hidden" }}
+								encType="multipart/form-data"
+							>
+								<input
+									ref={fileInputRef}
+									type="file"
+									name="Select from computer"
+									onChange={handleFileInputChange}
+								/>
+							</form>
+						</div>
+					) : // preview
+					selectedFile && preview ? (
+						<Preview
+							preview={preview}
+							name={selectedFile.name}
+							type={selectedFile.type}
+						/>
+					) : null}
+				</div>
+				{fileAdded ? (
+					<div
+						className="space-y-3 border-l p-3"
+						style={{
+							borderColor: "#3d3d3d",
+						}}
+					>
+						<AvatarUsername
+							height={30}
+							width={30}
+							className="text-sm font-bold text-white"
+						/>
+						<textarea
+							name="caption"
+							id="caption"
+							cols={30}
+							rows={10}
+							placeholder="Write a caption..."
+							className="resize-none bg-transparent outline-none"
+							required={false}
+							onChange={(e) => setCaption(e.target.value)}
+						></textarea>
+						<SmileIcon width={20} height={20} />
+						<div className="flex justify-between">
+							<input
+								type="text"
+								placeholder="Add location"
+								className="border-none bg-transparent outline-none"
+								onChange={(e) => setLocation(e.target.value)}
+							/>
+							<span>
+								<LocationIcon />
+							</span>
+						</div>
+					</div>
+				) : null}
+			</div>
+		</>
+	);
+}
+
+export default post;
