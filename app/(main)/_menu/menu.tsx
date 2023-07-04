58c552308bd873a2a50ac04c60b5b1e93a828176
AnanthuTD <ananthutd2021@gmail.com>
2023-07-04 19:13:51 +0530

    svg icons replaced by imported icon components.

--- app/(main)/_menu/menu.tsx
+++ app/(main)/_menu/menu.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -8,7 +9,9 @@ import { useUserContext } from "@/app/components/context/userContext";
 import Logo from "@/app/components/logo";
 import Reel from "@/app/components/icons/Reel";
 import Search from "@/app/components/icons/Search";
+import CreateIcon from "@/app/components/icons/Create";
 import Create from "@/app/(main)/_create/create";
+import MessageIcon from "@/app/components/icons/Message"
 
 export default function Menu() {
 	const { menu, HandleSetMenu } = useMenuContext();
@@ -17,31 +20,55 @@ export default function Menu() {
 
 	const [create, setCreate] = useState(false);
 
+	// const mainRef = useRef<HTMLDivElement>(null);
+
+	// function handleMenuBarVisibility(): void {
+	/**
+	 * if screen size lessthan 1024(lg) and not in home page hide the menubar
+	 *
+	 * @description This function hides the menu bar if the window width is less than 1024 and the current page is not the home page.
+	 */
+
+	/* 	// const currentRoutePath = router.;
+		const currentRoutePath=window.location.pathname
+
+		if (!mainRef.current?.parentElement || currentRoutePath === "/") return;
+
+		const width = window.innerWidth;
+
+		// If the window is less than a certain width, hide the menu bar.
+		if (width < 1024) {
+			mainRef.current.parentElement.style.display = "none";
+		} else {
+			mainRef.current.parentElement.style.display = "block";
+		}
+	} */
+
+	/* useEffect(() => {
+		handleMenuBarVisibility();
+		window.addEventListener("resize", handleMenuBarVisibility);
+
+		// Cleanup the event listener when the component unmounts
+		return () => {
+			handleMenuBarVisibility();
+			window.removeEventListener("resize", handleMenuBarVisibility);
+		};
+	}, []); */
+
 	return (
 		<>
-			<div className="text-primaryText lg:pt-5 ">
+			<div className="text-primaryText lg:pt-5" /* ref={mainRef} */>
 				<div className="mb-10 hidden xl:block">
 					<Logo />
 				</div>
 				<div>
 					<ul className="text-md flex list-none max-lg:justify-between lg:block lg:space-y-4">
-						<li onClick={() => HandleSetMenu("home")} className="order-1 lg:order-none">
+						<li
+							onClick={() => HandleSetMenu("home")}
+							className="order-1 lg:order-none">
 							<Link
 								href={"/"}
 								className="flex h-8 cursor-pointer items-center space-x-2 rounded-lg px-2 py-6 hover:bg-side_bar_hover">
-								{/* 	<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="currentColor"
-									className="h-6 w-6">
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-									/>
-								</svg> */}
 								<svg
 									aria-label="Home"
 									className="relative block"
@@ -67,7 +94,9 @@ export default function Menu() {
 								</p>
 							</Link>
 						</li>
-						<li onClick={() => HandleSetMenu("search")} className="order-2 lg:order-none">
+						<li
+							onClick={() => HandleSetMenu("search")}
+							className="order-2 lg:order-none">
 							<Link
 								href={"/search"}
 								className="flex h-8 cursor-pointer items-center space-x-2 rounded-lg px-2 py-6 hover:bg-side_bar_hover">
@@ -101,7 +130,9 @@ export default function Menu() {
 								</p>
 							</Link>
 						</li>
-						<li onClick={() => HandleSetMenu("reels")} className="order-4 lg:order-none">
+						<li
+							onClick={() => HandleSetMenu("reels")}
+							className="order-4 lg:order-none">
 							<Link
 								href={"/reel"}
 								className="flex h-8 cursor-pointer items-center space-x-2 rounded-lg px-2 py-6 hover:bg-side_bar_hover">
@@ -121,19 +152,7 @@ export default function Menu() {
 							<Link
 								href={"/inbox"}
 								className="flex h-8 cursor-pointer items-center space-x-2 rounded-lg px-2 py-6 hover:bg-side_bar_hover">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="currentColor"
-									className="h-6 w-6">
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
-									/>
-								</svg>
+								<MessageIcon/>
 								<p
 									className={[
 										menu.messages ? "font-bold" : "",
@@ -172,58 +191,9 @@ export default function Menu() {
 							</Link>
 						</li>
 						<li
-							className="order-3 lg:order-none flex h-8 cursor-pointer items-center space-x-2 rounded-lg px-2 py-6 hover:bg-side_bar_hover"
+							className="order-3 flex h-8 cursor-pointer items-center space-x-2 rounded-lg px-2 py-6 hover:bg-side_bar_hover lg:order-none"
 							onClick={() => setCreate(true)}>
-							{/* <svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth={1.5}
-								stroke="currentColor"
-								className="h-6 w-6">
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-								/>
-							</svg> */}
-							<svg
-								aria-label="New post"
-								className="relative block"
-								color="rgb(245, 245, 245)"
-								fill="rgb(245, 245, 245)"
-								height="24"
-								role="img"
-								viewBox="0 0 24 24"
-								width="24">
-								<path
-									d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552Z"
-									fill="none"
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"></path>
-								<line
-									fill="none"
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									x1="6.545"
-									x2="17.455"
-									y1="12.001"
-									y2="12.001"></line>
-								<line
-									fill="none"
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									x1="12.003"
-									x2="12.003"
-									y1="6.545"
-									y2="17.455"></line>
-							</svg>
+							<CreateIcon />
 							<p
 								className={[
 									menu.create ? "font-bold" : "",
@@ -233,7 +203,9 @@ export default function Menu() {
 							</p>
 							{create ? <Create setCreate={setCreate} /> : null}
 						</li>
-						<li onClick={() => HandleSetMenu("profile")} className="order-5 lg:order-none">
+						<li
+							onClick={() => HandleSetMenu("profile")}
+							className="order-5 lg:order-none">
 							<Link
 								href={"/profile"}
 								className="flex h-8 cursor-pointer items-center space-x-2 rounded-lg px-2 py-6 hover:bg-side_bar_hover">
