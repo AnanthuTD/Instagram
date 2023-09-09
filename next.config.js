ccd264995ab41768bf04b59c5e5fefd9f4d6485d
Ananthu TD <ananthutd2021@gmail.com>
2023-09-09 10:25:46 +0530

    ngrok

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ const proxy = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/:path*/',
+        // destination: 'http://127.0.0.1:8000/:path*/',
+        destination: ' https://5477-2402-3a80-1e0a-8016-8511-a8d0-263a-dd55.ngrok-free.app/:path*/',
       },
     ];
   },
