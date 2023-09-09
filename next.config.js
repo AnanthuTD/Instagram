47b56b3a031d11c6a951056abb77fede2f8ca92b
Ananthu TD <ananthutd2021@gmail.com>
2023-09-09 10:59:12 +0530

    .

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const proxy = {
       {
         source: '/api/:path*',
         // destination: 'http://127.0.0.1:8000/:path*/',
-        destination: 'https://5477-2402-3a80-1e0a-8016-8511-a8d0-263a-dd55.ngrok-free.app/:path*/',
+        destination: 'https://ad16-2402-3a80-1e0a-8016-1811-b7e0-e5c8-cc92.ngrok-free.app/:path*/',
       },
     ];
   },
