b3ba379c187373bc69e32a81b94cb5718838436f
Ananthu TD <ananthutd2021@gmail.com>
2024-05-20 09:08:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const proxy = {
 		return [
 			{
 				source: "/api/:path*",
-				destination: "http://127.0.0.1:8000/:path*/",
+				destination: `${process.env.API_ENDPOINT}/:path*/`,
 				// destination: 'https://14c6-2402-3a80-1e0a-8016-1811-b7e0-e5c8-cc92.ngrok-free.app/:path*/',
 			},
 		];
